794d0d37088589172f222f3c37c53e3c0b8c2265
elilourenco <lourencomonica318@gmail.com>
2025-09-16 20:59:53 +0100

    add the update for the code

---
+++ src/app/message/page.tsx
@@ -0,0 +1,34 @@
+
+export default function Message(){
+    return(
+        <div className="bg-zinc-600 justify-center">
+          <h1>message layout</h1>
+
+            <div className="focus-within:outline-2 focus-within:outline-indigo-600 ...">
+                <textarea className="outline-none ..." placeholder="Leave a comment..." />
+                <button className="..." type="button">Post</button>
+            </div>
+            <div className="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+        <div>
+    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
+      <svg
+        className="h-6 w-6 text-white"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        
+      </svg>
+    </span>
+  </div>
+  <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
+  <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+  </p>
+</div>
+
+            
+        </div>
+    )
+}
---
+++ src/app/planetas/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function planetas (){
+    return(
+        <div className="justify-center">
+            <h1 className="text-amber-50  justify-center">planetas</h1>
+            <p>Descrição do planeta terra</p>
+
+        </div>
+
+        
+    )
+}
